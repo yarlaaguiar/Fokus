b68776224da385727d4ad66034345630fd76700d
Yarla Aguiar <yarla.aguiar@gmail.com>
2024-07-01 19:09:00 -0300

    srci

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-const HTML = document.querySelector('html')
-const focoBt = document.querySelector('.app__card-button--foco active')
+// variáveis 
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 
-focoBt.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', 'imagens/foco.png')
+    focoBt.addEventListener('click', () =>{
+     alterarContexto('foco')
 })
-curtoBt.addEventListener('click', () =>{
-        html.setAttribute('data-contexto', 'descanso-curto')
-        banner.setAttribute('src', 'imagens/descanso-curto.png')
+    curtoBt.addEventListener('click', () =>{
+        alterarContexto('descanso-curto')
 })
-banner.addEventListener('click', () => {
-    html.setAttribute('data contexto', 'descanso--longo')
-    banner.setAttribute('src', 'imagens/descanso-longo.png' )
-})
+    longoBt.addEventListener('click', ()=>{
+        alterarContexto('descanso-longo')
+    } )
+    function alterarContexto(contexto) {
+        html.setAttribute('data-contexto', contexto)
+        banner.setAttribute ('src', `imagens/${contexto}.png`)
+        switch (contexto) {
+            case "foco":
+                titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+                break;
+            case "descanso-curto":
+                titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+                break;
+            case "descanso-longo":
+                titulo.innerHTML = `Hora de voltar à superfície.
+<strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            default:
+                break;
+        }
+}
